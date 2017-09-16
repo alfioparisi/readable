ce4925c6ac23cdd4547c4084a95c8285b1751c3f
Alfio Parisi <alfioparisi.93@gmail.com>
2017-09-16 19:20:04 +0100

    Get react-router-dom. Write basic home page.

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,46 @@ class App extends Component {
   render() {
     return (
       <div>
-        {JSON.stringify(this.props.store.getState())}
+        <header>
+          <div>
+            <button>Nav button</button>
+            <h1>Readable</h1>
+          </div>
+          <nav>
+            <ul>
+              <li>Category1</li>
+              <li>Category2</li>
+              <li>Category3</li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <header>
+            <h2>Web app for Udacity React Nanodegree</h2>
+          </header>
+          <p>You can either create an account or move on as 'Anonymous'</p>
+          <div>
+            <a href="#signup">Sign Up</a>
+            <a href="#login">Log In</a>
+            <a href="#allcategories">Anonymous</a>
+          </div>
+        </main>
+        <footer>
+          <nav>
+            <ul>
+              <li>Category1</li>
+              <li>Category2</li>
+              <li>Category3</li>
+            </ul>
+          </nav>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Sign Up</li>
+              <li>Log In</li>
+            </ul>
+          </nav>
+        </footer>
       </div>
     );
   }
